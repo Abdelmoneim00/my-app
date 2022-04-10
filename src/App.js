04029df044ff5453168233a35d018f10c2f9b4bc
abdelmoneim <ab2arabi22@gmail.com>
2022-04-10 12:29:20 +0200

    start working on about-me section and replacing the design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
-import Header from "./Header"
+import AboutMe from "./Components/AboutMe";
+import Header from "./Components/Header"
 function App() {
   return (
     <div>
       <Header />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/Components/AboutMe.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+function AboutMe() {
+    return (
+    <section className="About-me">
+        <div className="container">
+            <h1>About me</h1>
+            <div className="info">
+                <h3>Name :</h3>
+                <p>Abdel-Moneim Arabi Ibrahim (you can call me Abdu)</p>
+                <h3>Job Title :</h3>
+                <p>front-end developer</p>
+            </div>
+        </div>
+    </section>    
+    )
+}
+
+export default AboutMe
