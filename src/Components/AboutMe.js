25a07d8ca358acf9e01709a4582df96dc39b20b1
abdelmoneim <ab2arabi22@gmail.com>
2022-05-11 00:30:07 +0200

    checkpoint for about me section

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -6,42 +6,17 @@ import southKorea from "../images/southKorea.jpg"
 
 
 function AboutMe() {
-    const [image, setImage] = useState(NewYork);
-  const [imageIndex, setImageIndex] = useState(0);
-  const imgArray = [NewYork, Tokyo, Canada, southKorea];
-
-  let i = 0;
-  const maxImgArray = imgArray.length - 1;
-
-  useEffect(() => {
-    let interval = null;
-    if (i !== maxImgArray) {
-      interval = setInterval(() => {
-        let image = imgArray[imageIndex];
-        const attr = document.getElementById("moving-image");
-        attr.src = image;
-        // update the img index to state
-        setImageIndex((imageIndex) =>
-          imageIndex === maxImgArray ? 0 : imageIndex + 1
-        );
-        // update the src in state.
-        setImage(attr.src);
-      }, 5000);
-    }
-    // When our code runs and reruns for every render, useEffect also cleans up after itself using the cleanup function.
-    // Here we clear the interval to remove the effects that could happen with state change while interval.
-    return () => clearInterval(interval);
-  }, [image, imageIndex]); // when image, imageIndex gets change, useEffect will be triggered.
-
-
     return (
     <section className="About-me">
         <h1>About me</h1>
-        <div className="container">
-        <div className="cities-images">
-            <img 
-            src={Tokyo}
-            alt="NewYork" id="moving-image"></img>
+        <div className="container" id="h">
+        <div className="hoppies-images">
+            <img src={Tokyo} alt="Tokyo image" id="moving-image"></img>
+            <div className="middle-images">
+                <img src={NewYork} alt='NewYork image' id="middle-1"></img>
+                <img src={southKorea} alt="South Korea" id="middle-2"></img>
+            </div>
+            <img src={Canada} alt="Canada"></img>
         </div>
             <div className="info">
                 <p>Abdel-moneim Arabi Ibrahim</p>
