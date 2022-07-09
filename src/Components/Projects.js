eba04f6531ce2ee23aeec7a0347db488aa8e5bc9
Abdel-Moneim Arabi <ab000arabi22@gmail.com>
2022-07-09 18:35:27 +0200

    Project-Component Start making the Project-Component and styling the section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react"
-import Bot from "../images/BotImage.png"
+import discord from "../images/discord-log.png"
+
 
 function  Projects() {
 
@@ -12,7 +13,11 @@ function  Projects() {
                     <h3>Projects i'm proud of.</h3>
                 </div>
             <div className="Projects-containter">
-                
+                <div className="Project-component">
+                    <div className="image-wrapper">
+                        <img src={discord}></img>
+                    </div>
+                </div>
             </div>
         </section>
 
